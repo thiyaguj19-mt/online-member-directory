c82ac21c786d760c581071f136f511b2b9082225
thiyagarajan mohan <thiyaguj19@live.com>
2022-02-01 15:54:17 -0800

    modified allowed_hosts config

--- online_member_dir/settings.py
+++ online_member_dir/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['online-member-directory.herokuapp.com']
 
 
 # Application definition
