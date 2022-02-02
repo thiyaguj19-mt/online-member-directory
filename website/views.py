2e24eed7d84db4e1e4d46cac7d39bd9b6fdc2eb1
thiyagarajan mohan <thiyaguj19@live.com>
2022-02-01 20:00:34 -0800

    made changes views.py urls.py settings.py and website folder with html files

--- website/views.py
+++ website/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello World")
+# def index(request):
+#     return HttpResponse("Hello World")
+
+#This is the front home page screen view
+
+def home(request):
+    return render(request,'home.html',{})    
