566eddb0a6e257689b5f588dbf77f45cc72386ac
thiyagarajan mohan <thiyaguj19@live.com>
2022-02-15 18:57:08 -0800

    made center, org_role to be text field in filter

--- website/filters.py
+++ website/filters.py
@@ -6,7 +6,12 @@ from .models import Member
 
 class MemberFilter(django_filters.FilterSet):
 
+    first_name = django_filters.CharFilter(lookup_expr='icontains')
+    #last_name = django_filters.CharFilter(lookup_expr='icontains')
+    center__name = django_filters.CharFilter(lookup_expr='icontains')
+    orgrole__name = django_filters.CharFilter(lookup_expr='icontains')
+
     class Meta:
         model = Member
         #fields = "__all__"
-        fields = ['orgrole', 'member_status', 'region', 'center']
+        fields = ['first_name', 'age_group', 'member_status', 'region']
