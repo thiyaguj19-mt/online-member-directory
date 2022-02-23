294cfa5fe98b3b0c40ce69404c935d2870e83abb
thiyagarajan mohan <thiyaguj19@live.com>
2022-02-22 20:17:50 -0800

    added feature to update profile and member status

--- static/js/script.js
+++ static/js/script.js
@@ -11,3 +11,95 @@ const displayFilter = () => {
 		}
 	}
 }
+
+const showEditUI = (emailid) => {
+	const first_name = document.getElementById("first_name");
+	const last_name = document.getElementById("last_name");
+	const emailaddr = document.getElementById("emailaddr");
+	const orgrole = document.getElementById('m_id_orgrole');
+	console.log("orgrole" + orgrole.selectedIndex);
+	var url = 'getMemberData/';
+	fetch(url, {
+		method:'GET',
+		headers:{
+			'Content-Type':'application/json',
+			'X-CSRFToken':csrftoken,
+			'emailId': emailid,
+		},
+	})
+	.then((response) => {
+		return response.json();
+	 })
+	 .then((jsondata) => {
+		console.log(jsondata)
+		memberdata = JSON.parse(jsondata);
+		memberfields = memberdata[0].fields;
+		first_name.value = `${memberfields.first_name}`;
+		last_name.value	= `${memberfields.last_name}`;
+		emailaddr.innerHTML	= `${emailid}`;
+		selectedval = memberfields.orgrole.filter((val) => val);
+		$("#m_id_orgrole").val(selectedval);
+	 })
+}
+
+const saveChanges = () => {
+
+	const first_name = document.getElementById("first_name");
+	const last_name = document.getElementById("last_name");
+	const emailaddr = document.getElementById("emailaddr");
+	//const orgrole = document.getElementById("m_id_orgrole");
+	console.log("first_name " + first_name.value);
+	console.log("last_name " + last_name.value);
+	console.log("emailaddr " + emailaddr.innerHTML);
+
+	var url = 'updateMemberProfile/';
+	fetch(url, {
+		method:'POST',
+		headers:{
+			'Content-Type':'application/json',
+			'X-CSRFToken':csrftoken,
+		}, 
+		body:JSON.stringify({
+			'first_name': first_name.value, 
+			'last_name': last_name.value,
+			'emailaddr': emailaddr.innerHTML,
+			'orgrole': $('#m_id_orgrole').val(),
+		})
+	})
+	.then((response) => {
+		return response.json();
+		})
+	.then((data) => {
+		location.reload()
+	});
+}
+
+const updateMemberStatus = (emailId, val, message, bodymessage) => {
+
+	const status_label = document.getElementById(`${emailId}_member_status`);
+	var url = 'updateMemberStatus/';
+	fetch(url, {
+		method:'POST',
+		headers:{
+			'Content-Type':'application/json',
+			'X-CSRFToken':csrftoken,
+		}, 
+		body:JSON.stringify(bodymessage)
+	})
+	.then((response) => {
+		return response.json();
+		})
+	.then((data) => {
+		status_label.innerHTML = message
+		console.log("updated successfully" + data);
+	});
+}
+
+const setMemberStatus = (emailId) => {
+	const member_status = document.getElementById(`${emailId}_member_status`);
+	if (member_status.innerHTML === 'Approved' ) {
+		updateMemberStatus(emailId, 0, 'Pending_Approval', {"member_status": "0", 'emailaddr': emailId});
+	} else {
+		updateMemberStatus(emailId, 1, 'Approved', {"member_status": "1", 'emailaddr': emailId});
+	}
+}
--- website/admin.py
+++ website/admin.py
@@ -4,9 +4,14 @@ from .models import *
 # Register your models here.
 #admin.site.register(Region)
 #admin.site.register(Center)
-admin.site.register(OrgRole)
+#admin.site.register(OrgRole)
 #admin.site.register(AppRole)
 #admin.site.register(Member)
+
+class OrgRoleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+admin.site.register(OrgRole, OrgRoleAdmin)
 
 class RegionAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
